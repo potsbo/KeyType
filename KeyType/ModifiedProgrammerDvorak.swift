be572eaf1fa86081797b9ff871dbf1cb629b8073
Shimpei Otsubo <potsbo@gmail.com>
2020-08-23 02:23:50 +0900

    Merge two classes into one

--- KeyType/ModifiedProgrammerDvorak.swift
+++ KeyType/ModifiedProgrammerDvorak.swift
@@ -8,9 +8,33 @@
 
 import Foundation
 
-class DefaultConfiguration: KeyMapConfiguration {
-    override init() {
-        super.init()
+class DefaultConfiguration {
+    var keyMappingList: [KeyEventMap] = []
+
+    var mapList: [CGKeyCode: [KeyEventMap]] {
+        if needsRehashing { rehash() }
+        return storedList
+    }
+
+    private var storedList: [CGKeyCode: [KeyEventMap]] = [:]
+    private var needsRehashing = true
+
+    private func rehash() {
+        storedList = [:]
+
+        for val in keyMappingList {
+            let key = val.input.keyCode
+
+            if storedList[key] == nil {
+                storedList[key] = []
+            }
+
+            storedList[key]?.append(val)
+        }
+        needsRehashing = false
+    }
+
+    init() {
         setMappingList()
     }
 
